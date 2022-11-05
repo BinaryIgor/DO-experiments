e30ed1ceb1e94c8a08383944eadeaf7a57cea9a7
Igor <iroztropinski@gmail.com>
2022-11-05 10:55:59 +0100

    add frontend

---
+++ vue-app/README.md
@@ -0,0 +1,17 @@
+## First - install dependencies
+
+If file "package-lock.json" is already present:
+
+`npm ci`
+
+Else
+
+`npm i`
+
+## Run locally
+
+`npm run dev`
+
+## Build
+
+`npm run build`
---
+++ vue-app/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<body>
+    Some regular vue app!
+</body>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+export default defineComponent({
+});
+</script>
