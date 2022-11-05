f5b86b599a6afe4931445a643562050e743587e0
Igor <iroztropinski@gmail.com>
2022-11-05 11:57:44 +0100

    Add basic java app

---
+++ java-app/src/main/java/com/igor101/dojavaexperiments/DoJavaExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.igor101.dojavaexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoJavaExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoJavaExperimentsApplication.class, args);
+	}
+
+}
---
+++ java-app/src/test/java/com/igor101/dojavaexperiments/DoJavaExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor101.dojavaexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoJavaExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
