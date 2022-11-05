0326a65e80a9a834aee13ff3163d46255bd805ff
Igor <iroztropinski@gmail.com>
2022-11-05 10:30:04 +0100

    Another commit

--- node-app/index.js
+++ node-app/index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8080;
 const secretParam = process.env.SECRET_PARAM || "default X";
 
 const server = http.createServer((req, res) => {
+    console.log("Getting request...", req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', "text/plain");
     res.end(`Just some node server, asked at: ${new Date()}, with env: ${secretParam}`);
